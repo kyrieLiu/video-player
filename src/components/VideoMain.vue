7c0779b70c0ec1f918a89ca88916daa656d3e82a
yang1.zhang <zhangyang1@jingcaiwang.cn>
2019-08-23 13:42:35 +0800

    录入视频目录数据

--- src/components/VideoMain.vue
+++ src/components/VideoMain.vue
@@ -1,66 +1,84 @@
+<!--
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-08-23 10:33:10
+ * @LastEditTime: 2019-08-23 13:40:11
+ * @LastEditors: Please set LastEditors
+ -->
 <template>
-  <div style="height: 100%;display: flex;">
-
-    <div style="float: left; background: black;width: 300px;height: 100%;">
-
-        <div class="video-menu" v-for="(item,index) in videoList" v-bind:key="index" @click="clickMenu(item,index)"
-             :class="{'selectMenu':selectIndex===index}">
-          {{item.name}}
-        </div>
+  <div  style="height: 100%;display: flex;">
+    <div  style="float: left; background: black;width: 300px;height: 100%;overflow: auto">
+      <div
+        class="video-menu"
+        v-for="(item,index) in videoList"
+        v-bind:key="index"
+        @click="clickMenu(item,index)"
+        :class="{'selectMenu':selectIndex===index}"
+      >{{item.name}}</div>
     </div>
 
     <div style="float: left;background: aquamarine;width: 100%">
-<!--      <video id="video" src="https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBLF1eSL2AKx9lAGzGJXfnZnE784.mp4"
-             controls width="100%" height="100%"></video>-->
+      <!--      <video id="video" src="https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBLF1eSL2AKx9lAGzGJXfnZnE784.mp4"
+      controls width="100%" height="100%"></video>-->
       <video id="video" width="100%" height="100%" controls="controls">
-        <source src="https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBLF1eSL2AKx9lAGzGJXfnZnE784.mp4" type="video/ogg" />
-        <source src="https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBLF1eSL2AKx9lAGzGJXfnZnE784.mp4" type="video/mp4" />
-        <source src="https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBLF1eSL2AKx9lAGzGJXfnZnE784.mp4" type="video/webm" />
-        <object data="https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBLF1eSL2AKx9lAGzGJXfnZnE784.mp4" width="100%" height="100%">
+        <source
+          src="https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBLF1eSL2AKx9lAGzGJXfnZnE784.mp4"
+          type="video/ogg"
+        />
+        <source
+          src="https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBLF1eSL2AKx9lAGzGJXfnZnE784.mp4"
+          type="video/mp4"
+        />
+        <source
+          src="https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBLF1eSL2AKx9lAGzGJXfnZnE784.mp4"
+          type="video/webm"
+        />
+        <object
+          data="https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBLF1eSL2AKx9lAGzGJXfnZnE784.mp4"
+          width="100%"
+          height="100%"
+        >
           <embed width="100%" height="100%" src="/i/movie.swf" />
-
         </object>
       </video>
     </div>
-
   </div>
 </template>
 
 <script>
-import {videoList} from '../lib/videopath'
+import { videoList } from "../lib/videopath";
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
       selectIndex: 0,
       videoList: []
-    }
+    };
   },
-  mounted () {
-    this.videoList = videoList
+  mounted() {
+    this.videoList = videoList;
   },
   methods: {
-    clickMenu (item, index) {
-      this.selectIndex = index
-      let url = item.url
-      document.querySelector('#video').src = url
+    clickMenu(item, index) {
+      this.selectIndex = index;
+      let url = item.url;
+      document.querySelector("#video").src = url;
     }
   }
-}
+};
 </script>
 
 <style scoped>
-  .video-menu{
-    color: rgb(120 125 130);
-    cursor: pointer;
-    text-align: center;
-    line-height: 50px;
-  }
-  .video-menu:hover{
-    color: rgb(23 130 59);
-  }
-  .selectMenu{
-    color: rgb(23 130 59) ;
-  }
-
+.video-menu {
+  color: rgb(120 125 130);
+  cursor: pointer;
+  text-align: center;
+  line-height: 50px;
+}
+.video-menu:hover {
+  color: rgb(23 130 59);
+}
+.selectMenu {
+  color: rgb(23 130 59);
+}
 </style>
--- src/lib/videopath.js
+++ src/lib/videopath.js
@@ -1,4 +1,95 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-08-23 10:33:10
+ * @LastEditTime: 2019-08-23 13:38:41
+ * @LastEditors: Please set LastEditors
+ */
 export const videoList = [
   {name: '1.1课程简介', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBOF1eSLeAKzORAWhALR8db6M228.mp4'},
-  {name: '2.1课程学习方法.mp4', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBLF1eSL2AKx9lAGzGJXfnZnE784.mp4'},
-  {name: '2.2hello world.mp4', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBOF1eSNKAWZu-AY384vzWcRI849.mp4'}]
+  {name: '2.1课程学习方法', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBLF1eSL2AKx9lAGzGJXfnZnE784.mp4'},
+  {name: '2.2hello world', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBOF1eSNKAWZu-AY384vzWcRI849.mp4'},
+  {name: '2.3开发TodoList(vmodel,vfor,von)', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBLF1eSPaARzOBAoTzig2Ne8s707.mp4'},
+  {name: '2.4MVVM模式', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBOF1eSR6AWADwAtfav0bthnw007.mp4'},
+
+  {name: '2.5前端组件化', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBLF1eSSKAXuCVAE2qOKa13oQ945.mp4'},
+  {name: '2.6使用组件改造TodoList', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBOF1eSTqAUNAKAa8HLbtMX9c846.mp4'},
+  {name: '2.7简单的组件间传值', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBLF1eSVOAATAbAcaupBps6Bc881.mp4'},
+  {name: '2.8章节小结', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBOF1eSVaAe8QaADihhmww6f8088.mp4'},
+  {name: '3.1Vue实例', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBLF1eSYqABEALAU1aTpoukBw078.mp4'},
+
+  {name: '3.2Vue实例生命周期', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBOF1eSceAWqKBA6Yf62moG8g780.mp4'},
+  {name: '3.3Vue的模板语法', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBLF1eSduAEWu9AQlde4_Bg1E074.mp4'},
+  {name: '3.4计算属性，方法与侦听器', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBOF1eSgKAZ7mDAnhRo-xWRmo454.mp4'},
+  {name: '3.5计算属性的getter和setter', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBLF1eShKAXeT5ARXWq_cQRl4928.mp4'},
+  {name: '3.6Vue中的样式绑定', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBLF1eSkOAYRUxAwjzSBkL85Q733.mp4'},
+
+  {name: '3.7Vue中的条件渲染', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBOF1eSmqATcoXAnczva3zp_k992.mp4'},
+  {name: '3.8Vue中的列表渲染', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBLF1eSp-AKzCbAwwuU1mL6uQ018.mp4'},
+  {name: '3.9Vue中的set方法', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBOF1eSq6AcjhEANco1ucRk6k136.mp4'},
+  {name: '4-1使用组件的细节点', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBLF1eSyeACYluBCP6GuvQcMw293.mp4'},
+  {name: '4-2父子组件间的数据传递', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBOF1eS1GABE3nApiyXEd-eKU061.mp4'},
+
+  {name: '4-3组件参数校验与非props特性', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBLF1eS5yACr_RAx_27dsYoaw703.mp4'},
+  {name: '4-4给组件绑定原生事件', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBOF1eS7GAKomRAOFTlb5FRK0367.mp4'},
+  {name: '4-5非父子组件间的传值', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBLF1eS92ABbzyAkdK7yh-rSo759.mp4'},
+  {name: '4-6在Vue中使用插槽', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBOF1eTAuANGy4AsFJ2yDz_jM648.mp4'},
+  {name: '4-7作用域插槽', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBLF1eTB2AI-nIAR63QZ_9gPo734.mp4'},
+
+  {name: '4-8动态组件与v-once指令', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBOF1eTDmAQb-iAVsMJFzzkJY164.mp4'},
+  {name: '5-1Vue动画-Vue中Css动画原理', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBLF1eXuGAHa1hApKJPxobXK4907.mp4'},
+  {name: '5-2在Vue中使用animate.css库', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBOF1eXwOASjpXAe7LplRDGaY463.mp4'},
+  {name: '5-3在Vue中同时使用过渡和动画', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBLF1eXyWAAzkvAlYAXKfeSmY426.mp4'},
+  {name: '5-4Vue中的Js动画与Veloctity.js的结合', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBOF1eX1WANyolAx1ZuRPV_8I395.mp4'},
+
+  {name: '5-5Vue中多个元素或组件的过渡', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBLF1eX3KAboNBAdnEC5qa0jI999.mp4'},
+  {name: '5-6Vue中的列表过渡', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBOF1eX4WAMBFpAS3xAPp0r_8585.mp4'},
+  {name: '5-7Vue中的动画封装', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBLF1eX6CAc3q0AYXCzEwYObc172.mp4'},
+  {name: '5-8本章小结', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBOF1eX6yASbTNAMgacTnPEvU822.mp4'},
+  {name: '6-1Vue项目预热-环境配置', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBLF1eYD2ATvxECWQv5b0JbtE203.mp4'},
+
+  {name: '6-2Vue项目预热-项目代码介绍', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBOF1eYE-AV-_XATcL0ak13Ag731.mp4'},
+  {name: '6-3Vue项目预热-单文件组件与Vue中的路由', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBLF1eYXGAUbTeA87qUW4cNLc876.mp4'},
+  {name: '6-4Vue项目预热-单页应用VS多页应用', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBOF1eYYmARrIyAbCyBS4ZqQY958.mp4'},
+  {name: '6-5Vue项目预热-项目代码初始化', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/BF/rBMBLF1eYeWAW7LVBjS0fo0lNOA193.mp4'},
+  {name: '7-1Vue项目首页-header区域开发', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBOF1eYk-ASodKBjBoGmzZCSE905.mp4'},
+
+  {name: '7-2Vue项目首页-iconfont的使用和代码优化', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBLF1eYrSAYghQBxg_6RAvk7M810.mp4'},
+  {name: '7-3Vue项目首页-首页轮播图', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBOF1eYzaARyJRCPADaGWUJ2E590.mp4'},
+  {name: '7-4Vue项目首页-图标区域页面布局', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBLF1eY2yAHU_CA48RUleA4ek168.mp4'},
+  {name: '7-5Vue项目首页-图标区域逻辑实现', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBLF1eY-uAQzzRCNMM6FYWHtk394.mp4'},
+  {name: '7-6Vue项目首页-热销推荐组件开发', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBLF1eZxCAeLQwCCAy7w5sRMo760.mp4'},
+
+  {name: '7-7Vue项目首页-开发周末游组件', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBOF1eZ0WAAXQxA7-ayq4WJNk539.mp4'},
+  {name: '7-8Vue项目首页-使用axios发送ajax请求', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBLF1eZ5yAc-0sBjnlW6NsvW4136.mp4'},
+  {name: '7-9Vue项目首页-首页父子组组件间传值', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBOF1eZ96AYJ5_BKYL3c5bj0Q346.mp4'},
+  {name: '8-1Vue项目城市选择页-路由配置', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBLF1eaASAJ2nOArr3SaOZFvM844.mp4'},
+  {name: '8-2Vue项目城市选择页-搜索框布局', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBOF1eaB2AKo7PAdspd1d49Q0241.mp4'},
+
+  {name: '8-3Vue项目城市选择页-列表布局', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBLF1eaGmAZT0NBWJ8ie5rmDQ451.mp4'},
+  {name: '8-4Vue项目城市选择页-BetterScroll的使用和字母表布局', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBOF1eaJ2AZByvA2U0ULQlQjI152.mp4'},
+  {name: '8-5Vue项目城市选择页-页面的动态数据渲染', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBLF1eaPGAXZx1BTu5ltEZ1xk646.mp4'},
+  {name: '8-6Vue项目城市选择页-兄弟组件数据传递', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBOF1eb6qAY61BB-bvqAommYM682.mp4'},
+  {name: '8-7Vue项目城市选择页-列表性能优化', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBLF1eb8qAFgR0AkSiZtOGgoA776.mp4'},
+
+  {name: '8-8Vue项目城市选择页-搜索逻辑实现', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBOF1ecCCAB56kBhtE9YcLN64861.mp4'},
+  {name: '8-9Vue项目城市选择页-Vuex实现数据共享', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBLF1ecHmAexqmBlT1-37Oo64540.mp4'},
+  {name: '8-10Vue项目城市选择页-Vuex的高级使用及localStorage', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBOF1ecLyAHwBnBMFaaJA8VuE446.mp4'},
+  {name: '8-11Vue项目城市选择页-使用keep-alive优化网页性能', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBLF1ecQeAHWILBV5LjH0_Hc0091.mp4'},
+  {name: '9-1Vue项目详情页-动态路由和banner布局', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBOF1ecWeAXgE9BsXxUNsAS4Q488.mp4'},
+
+  {name: '9-2Vue项目详情页-公用图片画廊组件拆分', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBLF1eceyAMUu5CXoq0MlegtA709.mp4'},
+  {name: '9-3Vue项目详情页-实现Header渐隐渐显效果', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBLF1eclGAff8rBVF5q8vXkFU127.mp4'},
+  {name: '9-4Vue项目详情页-对全局事件的解绑', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBOF1ecmyAXbPcAfi0Pq9Kbkw124.mp4'},
+  {name: '9-5Vue项目详情页-使用递归组件实现详情页列表', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBLF1ecreAWuADBQwyPz8r7YY377.mp4'},
+  {name: '9-6Vue项目详情页-动态获取详情页数据', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBOF1ecxSAUkVLBiG8K7U4yRQ136.mp4'},
+
+  {name: '9-7Vue项目详情页-在项目中加入基础动画', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBLF1ec0GACsQlAtTH8B_cn48262.mp4'},
+  {name: '10-1Vue项目的联调测试上线-项目前后端联调', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBOF1ec1-ACEHRAf6GosaZLao521.mp4'},
+  {name: '10-2Vue项目的联调测试上线-真机测试', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBLF1ec7SAO9FYBe8sXdYS630515.mp4'},
+  {name: '10-3Vue项目的联调测试上线-打包上线', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBOF1ec--AKMBxA-28mCTgkNo512.mp4'},
+  {name: '10-4Vue的项目联调测试上线-异步组件实现按需加载', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBLF1edCqAOKzmBCy3HgFDt68875.mp4'},
+
+  {name: '10-5Vue项目的联调测试上线-课程总结与后续学习指南', url: 'https://security-test.jingcaiwang.cn/group1/M00/00/C0/rBMBOF1edDqAUdRwATB7W5nvjkA179.mp4'}
+
+]
